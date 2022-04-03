12ab516652b19a01c1f770cc01d4203ca5331811
hrb11182 <59706923+hrb11182@users.noreply.github.com>
2022-04-03 21:02:56 +0530

    first commit

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">
+      Designed by Himanshu Rai for final year project.
+    </div>
+  );
+};
+
+export default Footer;
